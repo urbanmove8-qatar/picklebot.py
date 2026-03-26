9e393d262d13eb5f32f1b09afa532a094a7d9a3d
Urbanmove 8 Team <robloxurbanmove8@gmail.com>
2026-03-26 12:58:01 +0300

    Add files via upload

---
+++ picklebot/__init__.py
@@ -0,0 +1,12 @@
+"""
+Picklebot.py
+~~~~~~~~~~~~~
+A modern, asynchronous Python wrapper for the Picklechat.net API.
+(c) 2026 Urbanmove 8 Hungária. All Rights Reserved
+MIT License
+"""
+
+from .core import Picklebot, PickleError, MessageResponse
+
+__version__ = "0.1.0"
+__author__ = "Urbanmove 8 Hungária"
